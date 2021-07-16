d0575d3deeda0885bc053f2c144d69e2ca819672
wzd200 <william.z.dye@gmail.com>
2021-07-16 15:03:38 -0400

    Added task completed, description and notes to getAll tasks

--- api/project/model.js
+++ api/project/model.js
@@ -3,21 +3,18 @@ const mappers = require('../../data/helpers/mappers')
 
 const getAll = async () => {
     const projectRows = await db('projects')
+        .orderBy('project_id')
 
     const projectsArray = []
 
     projectRows.map(projectRow => {
         const result = {
-            project_name: projectRow.project_name,
+            project_completed: mappers.intToBoolean(projectRow.project_completed),
             project_description: projectRow.project_description,
-            project_completed: mappers.intToBoolean(projectRow.project_completed)
+            project_name: projectRow.project_name
         }
         projectsArray.push(result)
-
     })
-
-    
-
     return projectsArray
 }
 
--- api/task/model.js
+++ api/task/model.js
@@ -1,9 +1,21 @@
 const db = require('../../data/dbConfig')
+const mappers = require('../../data/helpers/mappers')
 
-const getAll = () => {
-    const taskRows = db('tasks')
+const getAll = async () => {
+    const taskRows = await db('tasks')
+        .orderBy('task_id')
 
-    return taskRows
+    const tasksArray = []
+
+    taskRows.map(taskRow => {
+        const result = {
+            task_completed: mappers.intToBoolean(taskRow.task_completed),
+            task_description: taskRow.task_description,
+            task_notes: taskRow.task_notes
+        }
+        tasksArray.push(result)
+    })
+    return tasksArray
 }
 
 module.exports = {
