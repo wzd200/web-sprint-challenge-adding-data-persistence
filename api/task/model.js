982067384a7ae6af79c9e36497beefbaf9f68ef7
wzd200 <william.z.dye@gmail.com>
2021-07-16 15:13:27 -0400

    Added project description and name to task GET

--- api/task/model.js
+++ api/task/model.js
@@ -2,7 +2,8 @@ const db = require('../../data/dbConfig')
 const mappers = require('../../data/helpers/mappers')
 
 const getAll = async () => {
-    const taskRows = await db('tasks')
+    const taskRows = await db('tasks as t')
+        .join('projects as p', 't.project_id', 'p.project_id')
         .orderBy('task_id')
 
     const tasksArray = []
@@ -11,7 +12,10 @@ const getAll = async () => {
         const result = {
             task_completed: mappers.intToBoolean(taskRow.task_completed),
             task_description: taskRow.task_description,
-            task_notes: taskRow.task_notes
+            task_notes: taskRow.task_notes,
+            project_name: taskRow.project_name,
+            project_description: taskRow.project_description
+
         }
         tasksArray.push(result)
     })
