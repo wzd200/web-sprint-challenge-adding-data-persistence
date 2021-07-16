a59b45baed495e3a5b6263b482c43786184dce24
wzd200 <william.z.dye@gmail.com>
2021-07-16 16:59:08 -0400

    All tests passing

--- api/project/model.js
+++ api/project/model.js
@@ -4,7 +4,6 @@ const mappers = require('../../data/helpers/mappers')
 const getAll = async () => {
     const projectRows = await db('projects')
         .orderBy('project_id')
-
     const projectsArray = []
 
     projectRows.map(projectRow => {
@@ -21,33 +20,19 @@ const getAll = async () => {
 const getById = async (project_id) => {
     const projectRows = await db('projects')
         .where('project_id', project_id)
-
     const result = {
         project_completed: mappers.intToBoolean(projectRows[0].project_completed),
         project_description: projectRows[0].project_description,
         project_name: projectRows[0].project_name
     }
-
     return result
 }
 
 const add = async (project) => {
-    // insert data into db
     return db('projects')
         .insert(project)
         .then((project_id) => getById(project_id))
-
-
-    // return posted data with transformed completed status
 }
-
-// const add = (project) => {
-//     return db('projects').insert(project)
-//         .then(([project_id]) => {
-//             return db('projects')
-//                 .where('project_id', project_id).first()
-//         })
-// }
 
 module.exports = {
     getAll,
--- api/task/model.js
+++ api/task/model.js
@@ -22,13 +22,32 @@ const getAll = async () => {
     return tasksArray
 }
 
-const add = (task) => {
-    return db('tasks').insert(task)
-        .then(([task_id]) => {
-            return db('tasks')
-                .where('task_id', task_id).first()
-        })
+const getById = async (task_id) => {
+    const taskRows = await db('tasks')
+        .where('task_id', task_id)
+    const result = {
+        task_completed: mappers.intToBoolean(taskRows[0].task_completed),
+        task_description: taskRows[0].task_description,
+        task_notes: taskRows[0].task_notes,
+        project_name: taskRows[0].project_name,
+        project_description: taskRows[0].project_description
+    }
+    return result
 }
+
+const add = async (task) => {
+    return db('tasks')
+        .insert(task)
+        .then((task_id) => getById(task_id))
+}
+
+// const add = (task) => {
+//     return db('tasks').insert(task)
+//         .then(([task_id]) => {
+//             return db('tasks')
+//                 .where('task_id', task_id).first()
+//         })
+// }
 
 module.exports = {
     getAll,
